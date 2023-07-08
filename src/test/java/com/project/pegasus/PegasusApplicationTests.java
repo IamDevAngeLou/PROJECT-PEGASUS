41d874ba6a8dec60b2f783b019c21068953890b4
angelo51 <angelo@solutionsresource.com>
2023-07-08 15:24:41 +0800

    initial project commit for pegasus backend

---
+++ src/test/java/com/project/pegasus/PegasusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pegasus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegasusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
